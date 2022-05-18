d328b41b3817e714f95f2d61165c9f059a01ee45
GDiaz <gidiaz1@uc.cl>
2022-05-17 20:15:50 -0400

    Agregados parámetros

--- E2/sys.m
+++ E2/sys.m
@@ -1,4 +1,4 @@
-function dt = sys(~,x)
+function dt = sys(~,x,p)
 %Model states
 S = x(1);
 N = x(2);
@@ -10,15 +10,20 @@ qn = x(7);
 
 
 %Model parameters
-Sin = ; fsin = ; Yxs = ; Ylips = ; Ycits = ; Fin = ; Nin = ; fnin= ; rhon = ; Fsample = ;
-gamma = ; mumax = ; rhonmax = ; Q0 = ; Ks1 = ; kl1 = ; Kn = ; pilipmax = ; Ks2 = ; kl2 = ;
-alfa = ; k2 = ; qncrit = ; picitmax = ; Ks3 = ; kl3 = ; k3 = ;
+Yxs =p(4) ; Ylips = p(5); Ycits = p(6); 
+gamma = 0.06; mumax = p(1); rhonmax =p(16) ; Q0 =p(17) ; Ks1 = p(7); kl1 =p(13) ; Kn =p(10) ; pilipmax = p(2); Ks2 = p(8) ; kl2 =p(14) ;
+alfa = 0.36; k2 =p(11) ; qncrit = p(18); picitmax = p(3); Ks3 =p(9) ; kl3 =p(15) ; k3 = p(12);
+
+Sin =  p(19); fsin =p(20) ;  Nin =p(21) ; fnin=p(22) ; Fsample = 0;
+
+Fin = fsin + fnin;
 
 %Algebraic equations
 Xt = Xf*(1-gamma)+Xl;
 mu = mumax*(1-(Q0/qn))*(S/(Ks1+S+(S^2)/kl1));
 rhon = rhonmax*(N/(Kn+N));
 pilip = pilipmax*(S/(Ks2+S+(S^2)/kl2))*(1-Xl/(alfa*Xt))*(k2/(k2+CIT));
+
 
 if qn>qncrit
     indq = 0;
@@ -39,11 +44,8 @@ dqn = rhon-mu*qn;
 
 
 
-
-
-
-
 dt = [ds dn dxf dxl dcit dv dqn]';
 end
+
 
 
