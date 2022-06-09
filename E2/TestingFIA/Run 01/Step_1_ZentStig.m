176a3ec5a8d0b849eaa1263cc5f31ce18e9a0d05
Sebasthian Schnettler <ssschnettler@uc.cl>
2022-06-08 21:51:33 -0400

    FIA

---
+++ E2/TestingFIA/Run 01/Step_1_ZentStig.m
@@ -0,0 +1,130 @@
+
+%Adapted from https://sourceforge.net/projects/minimal-output-sets/
+%by Cristóbal Torrealba 2021
+
+%Application of the fast structural identifiability asessment applyed
+%to the Zenteno et al. (2010) wine fermentation model. 
+%Instructions for this example:
+
+    % Run step 1, wait until all variables are generated. If wanted,
+    % adjust the number of simulations (NExp); takes few seconds with
+    % Zenteno´s model
+    %
+    % Run step 2, wait until all simulations are performed. If wanted,
+    % adjust the bounds associated to random nominal parameter values
+    % perturbation; takes few minutes with Zenteno´s model.
+    %
+    % Run step 3, wait until all graphics are generated. If wanted,
+    % change measured states (sensors); takes a minute or so. 
+    % Enjoy.
+
+%% Initial procedures
+
+%Prepare computation for Zenteno model (NOTE: Parameter vector does not include initial conditions as unknown parameters!)
+tic
+ModelName='Samada';
+
+%Initial conditions for integration
+S       = 0.02; %Yeast dosis   (g/L)
+N       = 0.853; %Initial YAN   (g/L)
+Xf       = 8.1; %Initial Glu   (g/L)
+Xl       = 0.48; %Initial Fru   (g/L)
+CIT       = 0;      %Initial Eth   (g/L)
+V      = 8.26;
+qn   = 0.075;
+x0       = [S, N, Xf, Xl, CIT, V, qn];
+
+%Here operational data corresponding to this example is loaded
+load('Operational_Data.mat')
+
+%Define final integration time (Tf, in hours for this example) and
+%number of integration steps (Nt) 
+Tf=30.3; 
+Nt=450; 
+
+%% Symbolic Toolbox Variable defintions
+
+%Here, we define model states as x{i} for a total of i = 1..7 model states.
+for i=1:7
+    syms(sprintf('x%d',i),'real');
+end
+
+%Here, we define model parameters as th{i} for a total of i = 1..14 regression parameters.
+for i=1:18
+    syms(sprintf('th%d',i),'real');
+end
+
+%Additionally, we define variables t, U1(in this example, must temperature [K]), and U2 (logical value describing if yeast thermal death is ocurring) as real 
+%symbolic values representing time and model inputs.
+syms t real    
+syms U real
+
+%Once symbolic parameters and model states are defined, we build arrays to vectorize parameters and model states.
+statesSym=[x1 x2 x3 x4 x5 x6 x7]';
+thetaSym=[th1 th2 th3 th4 th5 th6 th7 th8 th9 th10 th11 th12 th13 th14 th15 th16 th17]'; %NOTE: DO NOT ADD FIXED PARAMETERS IN THETASYM!
+%         mu0 betaG0 betaF0 Kn0 Kg0 Kf0 Kig0 Kie0 Kd0 Yxn  Yxg  Yxf  Yeg  Yef
+
+%Define the amount of model states and parameters
+nx=length(statesSym);
+
+%Define initial conditions vector as a symbolic variable associated to numerical values.
+thIC=sym(x0);
+x0Sym=thIC';
+
+%Define the amount of simulations performed in the Monte Carlo procedure
+NExp=100; 
+
+%% Nominal parameter values (In this example, we use those in Zenteno et al. (2010))
+
+%NOTE: ¡Fixed parameter values must be removed!
+
+thetaNom = [  0.755                      %1) mu0
+              0.039                     %2) betaG0
+              0.314                     %3) betaF0
+              0.519                      %4) Kn0
+              0.272                      %5) Kg0
+              0.89                      %6) Kf0
+              1.116                      %7) Kig0
+              1.482                        %8) Kie0
+              12.22                   %9) Kd0
+              23.08                     %10) Yxn
+              9.75                      %11) Yxg
+              37.63                      %12) Yxf
+              83.64                      %13) Yeg
+              44.7
+              40.11
+              0.716
+              0.045];
+           %   0.047];   
+
+nth=length(thetaNom);
+
+%% MODEL DEFINITION
+
+%Here fixed parameter values must be defined:
+th18 = 0.047;
+
+%NOTE: This is the ODE system representing the Zenteno et al. (2010) model using no abreviatons for terms such as specific growth rate (mu).
+
+Xdot =  [
+            0*(0/x6)-(th1*(1-(th17/x7))*(x1/(th7+x1+(x1^2)/th13))/th4+th2*(x1/(th8+x1+(x1^2)/th14))*(1-x4/(0.36*x3*(1-0.06)+x4))*(th11/(th11+x5))/th5+th3*U*(x1/(th9+x1+(x1^2/th15)))*(th12/(th12+x5))/th6)*x3-(x1/x6)*0;                          
+            0*(0/x6)-th16*(x2/(th10+x2))*x3-(x2/x6)*0;                              
+            th1*(1-(th17/x7))*(x1/(th7+x1+(x1^2)/th13))*x3-(x3/x6)*0;    % Glucose
+            (th2*(x1/(th8+x1+(x1^2)/th14))*(1-x4/(0.36*x3*(1-0.06)+x4))*(th11/(th11+x5))+0.06*th1*(1-(th17/x7))*(x1/(th7+x1+(x1^2)/th13)))*x3-(x4/x6)*0;    
+            th3*U*(x1/(th9+x1+(x1^2/th15)))*(th12/(th12+x5))*x3-(x5/x6)*0;                       
+            0-0;
+            th16*(x2/(th10+x2))-th1*(1-(th17/x7))*(x1/(th7+x1+(x1^2)/th13))*x7];
+
+%% END MODEL DEFINITION AND AUXILIAR FUNCTION GENERATION
+%(May take a few minutes to process)
+
+%Here, gradients are calculated symbolically and automatically stored as separated Matlab scripts for later usage.
+dfdxSym     = simplify(jacobian(Xdot,statesSym));
+dfdthSym    = simplify(jacobian(Xdot,thetaSym));
+dx0dthSym   = simplify(jacobian(x0Sym,thetaSym));
+
+f = matlabFunction(Xdot,'vars',{t,statesSym,U,thetaSym},'file',ModelName);
+dfdx = matlabFunction(dfdxSym,'vars',{t,statesSym,U,thetaSym},'file',['dfdx',ModelName]);
+dfdth = matlabFunction(dfdthSym,'vars',{t,statesSym,U,thetaSym},'file',['dfdth',ModelName]);
+IC = matlabFunction(x0Sym,'vars',{thetaSym},'file',['x0',ModelName]);
+dICdth = matlabFunction(dx0dthSym,'vars',{statesSym,thetaSym},'file',['dICdth',ModelName]);
---
+++ E2/TestingFIA/t-values extended/HIPPO_modified/obj_func.m
@@ -0,0 +1,73 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% obj_func
+% Integrates the dynamic model and calculates afterwards the cuadratic 
+% difference between the model predictions and the experimental data,
+% normalized by the maximum value of the experimental data. Returns the
+% cuadratic difference (objective function). To be used in the parameter
+% estimation with SSm.
+%
+% Benjamín J. Sánchez
+% Last Update: 2013-06-07
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [J,g,R]=obj_func(k,~,ydata)
+
+Operational_Data = evalin('base','int_time');
+Measure_Ind      = evalin('base','Measure_Ind');
+time_dap         = evalin('base','time_dap');
+FDA_add_idx      = evalin('base','FDA_add_idx');
+x0               = evalin('base','x0');
+Kinetic_Matrix   = evalin('base','Kinetic_Matrix');
+
+%Definicion del tiempo de integracion en base a datos operacionales de seguimiento de temperatura
+int_time         = Operational_Data(:,1);
+
+%Definimos dos tiempos de integracion: Previo a la adicion de FDA y posterior a ello.
+t1        = int_time(int_time<time_dap);
+t2        = int_time(int_time>=time_dap);
+
+%Opciones de integracion
+options = odeset('RelTol',1e-3,'AbsTol',1e-3);
+
+%Integracion de la seccion previa a la adicion de FDA
+[T1,X1]   = ode15s(@model,t1,x0,options,k);
+
+%Re-definimos el punto inicial para la segunda seccion. (NOTA: El Nitrogeno fue medido tras la adicion de FDA, por ende se utiliza ese dato experimental para definir x02)
+x02       = X1(end,:);  x02(2) = ydata(FDA_add_idx,1);
+
+%Integracion de la seccion posterior a la adicion de FDA
+[T2,X2]   = ode15s(@model,t2,x02,options,k);
+
+%Concatenacion de los resultados obtenidos
+T         = [T1;T2];
+X         = [X1;X2];
+
+%Seleccion de los puntos objetivos (es decir, aquellos donde calzan los tiempos operacionales con los experimentales)
+
+X         = X(ismember(T,int_time),:);            %Eliminacion de los puntos extras generados de la integracion
+total_idx = (1:1:length(X))';                     %Vector de indices
+X_sim     = X(ismember(total_idx,Measure_Ind),:); %X_sim contiene los datos simulados que calzan con los puntos de muestreo experimental, por ende son comparables para el calculo de error.
+
+if length(X_sim(:,2)) == length(ydata(:,1)) %Condicional para evitar que eSS entregue soluciones inestables
+    
+    %Data definition
+    G_data                  = Kinetic_Matrix(:,1);                       G_sim                  = X_sim(:,3);
+    F_data                  = Kinetic_Matrix(:,2);                       F_sim                  = X_sim(:,4);
+    N_data                  = zeros(size(G_data));                       N_sim                  = zeros(size(G_data));
+    N_data(1:FDA_add_idx+1) = Kinetic_Matrix(1:FDA_add_idx+1,4);         N_sim(1:FDA_add_idx+1) = X_sim(1:FDA_add_idx+1,2).*1000;                                                                                      %N_pond    = 1/(sum(Kinetic_Matrixes{i,j}(1:FDA_add_idxs{i,j}+1,4))/sum(Kinetic_Matrixes{i,j}(:,3))); %This ponderator will weigth up N adjust to the same level as S related error.
+
+    %Construction of residual matrix:
+    R    = [(N_data-N_sim)./max(N_data) (G_data-G_sim)./max(G_data) (F_data-F_sim)./max(F_data)] ;
+    
+    %Calculation of the objective function:
+    J = sum(sum(R.^2)); %disp(['J = ' num2str(J)]);
+    g = 0;
+    R=reshape(R,numel(R),1);
+
+else    %Condicional para evitar que eSS entregue soluciones inestables
+    R    = NaN;
+    J    = NaN;
+    g    = NaN;
+end
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
